f81ae83b4eb4a54bb72151bff031071a9dbfe7bf
Steve Schwartz <steve@alfajango.com>
2011-03-03 22:56:07 -0500

    Updated controller with new responders. Removed old remotipart_response left over from other branch.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,34 +1,25 @@
 class CommentsController < ApplicationController
+  respond_to :html, :xml
+  respond_to :js, :only => [:create, :update, :destroy]
   # GET /comments
   # GET /comments.xml
   def index
     @comments = Comment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @comments }
-    end
   end
 
   # GET /comments/1
   # GET /comments/1.xml
   def show
     @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @comment }
-    end
   end
 
   # GET /comments/new
   # GET /comments/new.xml
   def new
     @comment = Comment.new
-
-    respond_to do |format|
-      format.html { render :new, :layout => ! request.xhr? } # new.html.erb
-      format.xml  { render :xml => @comment }
+    #respond_with(@comment, :layout => false)
+    respond_with do |format|
+      format.html { render :layout => ! request.xhr? }
     end
   end
 
@@ -40,47 +31,21 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.xml
   def create
-    @comment = Comment.new(params[:comment])
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to(@comment, :notice => 'Comment was successfully created.') }
-        format.xml  { render :xml => @comment, :status => :created, :location => @comment }
-        format.js
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
-        format.js
-      end
-    end
+    @comment = Comment.create(params[:comment])
   end
 
   # PUT /comments/1
   # PUT /comments/1.xml
   def update
     @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to(@comment, :notice => 'Comment was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
-      end
+    respond_with do |format|
+      format.html{ redirect_to @comment }
     end
   end
 
   # DELETE /comments/1
   # DELETE /comments/1.xml
   def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(comments_url) }
-      format.xml  { head :ok }
-      format.js
-    end
+    @comment = Comment.destroy(params[:id])
   end
 end
