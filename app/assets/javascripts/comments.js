db27757b2102907f7c2a1d535401d9a37d07d915
Steve Schwartz <steve@alfajango.com>
2011-07-11 17:19:42 -0400

    Upgraded from rails 3.0 to rails 3.1 with asset pipeline.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,6 +1,3 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
 $(document)
   .delegate('#new-comment-link, #new-comment-attachment-link', 'ajax:success', function(e, data, status, xhr){
     var $this = $(this),
