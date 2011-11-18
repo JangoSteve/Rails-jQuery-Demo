d48f2e5cb41a13e3baba86ed4e076cd5c5d3d6c3
Joel Nimety <jnimety@continuity.net>
2011-11-18 07:19:55 -0500

    add test showing data-type not required without attachment and required with attachment
    Signed-off-by: Joel Nimety <jnimety@continuity.net>

--- spec/integration/comments_spec.rb
+++ spec/integration/comments_spec.rb
@@ -127,12 +127,29 @@ describe 'comments' do
 
   it "allows json response", js: true do
     visit root_path
+    page.execute_script("$('form').live('ajax:success', function(evt, data, status, xhr) { $('#comments').after(data.json_response); });")
+    page.execute_script("$('form').live('ajax:error', function(evt, xhr, status, error) { $('#comments').after('Failure: ' + status); });")
+
+    click_link 'New Comment'
+
     page.execute_script("$('form').attr('action', '/comments.json');")
+    file_path = File.join(Rails.root, 'spec/fixtures/qr.jpg')
+    fill_in 'comment_subject', with: 'Hi'
+    fill_in 'comment_body', with: 'there'
+    click_button 'Create Comment'
+
+    page.should have_content('JSON response')
+  end
+
+  it "allows json response with attachment", js: true do
+    visit root_path
     page.execute_script("$('form').live('ajax:success', function(evt, data, status, xhr) { $('#comments').after(data.json_response); });")
     page.execute_script("$('form').live('ajax:error', function(evt, xhr, status, error) { $('#comments').after('Failure: ' + status); });")
 
     click_link 'New Comment with Attachment'
 
+    page.execute_script("$('form').attr('action', '/comments.json');")
+    page.execute_script("$('form').attr('data-type', 'json');")
     file_path = File.join(Rails.root, 'spec/fixtures/qr.jpg')
     fill_in 'comment_subject', with: 'Hi'
     fill_in 'comment_body', with: 'there'
