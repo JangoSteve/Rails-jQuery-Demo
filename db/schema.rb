1e66612adf4fecaacc87578676f6020317b11e95
Steve Schwartz <steve@alfajango.com>
2012-02-28 02:17:35 -0500

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110209210315) do
+ActiveRecord::Schema.define(:version => 20110714205346) do
 
   create_table "comments", :force => true do |t|
     t.string   "subject"
@@ -21,6 +22,10 @@ ActiveRecord::Schema.define(:version => 20110209210315) do
     t.string   "attachment_content_type"
     t.integer  "attachment_file_size"
     t.datetime "attachment_updated_at"
+    t.string   "other_attachment_file_name"
+    t.string   "other_attachment_content_type"
+    t.integer  "other_attachment_file_size"
+    t.datetime "other_attachment_updated_at"
   end
 
 end
