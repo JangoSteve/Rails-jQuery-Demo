e841c796d7292c006b19020d6777fad3aecaa220
Steve Schwartz <steve@alfajango.com>
2013-02-08 14:47:49 -0500

    Fixed test suite failing due to inconsistencies with db.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,9 +28,11 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
 
   config.use_transactional_fixtures = false
+  config.before :all do
+    DatabaseCleaner.clean
+  end
   config.before :each do
     DatabaseCleaner.start
   end
